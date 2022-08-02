619593f9c4b9084ac104d36f63ee1eaae00cebd7
amjadcp <amjadcp03@gmail.com>
2022-08-03 03:00:01 +0530

    doc

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,8 @@
 const basicInfo = require('./basicInfo');
+const login = require('./login')
 module.exports = {
     ...basicInfo,
+    paths: {
+        ...login
+    }
 };
--- models/adminSchema.js
+++ models/adminSchema.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose')
 
+// const obj = {
+//     name: {type: String},
+//     phoneNumber: {
+//         type: String,
+//         maxLength: 11,
+//         unique: true
+//     },
+//     role:{
+//         type: String,
+//         enum: {
+// 			values: ["admin", "ward-admin"],
+// 			message: "{VALUE} is not supported"
+// 		}
+//     },
+//     wardId: {type: mongoose.Schema.Types.ObjectId},
+//     wardNo: {type: String}
+// }
 const AdminSchema = new mongoose.Schema({
     name: String,
     phoneNumber: {
@@ -14,8 +31,9 @@ const AdminSchema = new mongoose.Schema({
 			message: "{VALUE} is not supported"
 		}
     },
-    wardId: mongoose.Schema.Types.ObjectId,
-    wardNo: String
+    wardId: {type: mongoose.Schema.Types.ObjectId},
+    wardNo: {type: String}
 },{timestamps:true})
 
 module.exports = mongoose.model('AdminSchema', AdminSchema)
+module.exports = AdminSchema
